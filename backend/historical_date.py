872e2d7d6b61b490ddf46c33a7cf71fff70a71db
Harper Noteboom <harper.noteboom@gmail.com>
2025-06-23 10:31:53 -0600

    update historical_date

--- backend/historical_date.py
+++ backend/historical_date.py
@@ -23,26 +23,19 @@ class HistoricalDate:
                 if match: 
                     self.year = -int(match.group(1)) # convert digits to year (negative for bc)
                     self.precision = "circa" if "circa" in text or "ca" in text or "c." in text else "exact"
-    
-        elif "circa" in text or "ca" in text or "c." in text: # handles circa dates that are not bc
-            if "century" in text: # ex: circa 19th century 
-                match = re.search(r"(\d+)(st|nd|rd|th)? century", text)
-                if match: 
-                    century = int(match.group(1))
-                    self.year = (century - 1 ) * 100 
-                    self.percision = "century"
-            else: 
-                match = re.search(r"(\d{1,4})", text) 
-                if match: 
-                    self.year = int(match.group(1))
-                    self.percision = "circa"
-        
-        elif "century" in text: # handles century with no circa: ex: 19th century 
+      
+        elif "century" in text: # handles century: ex: 19th century 
             match = re.search(r"(\d+)(st|nd|rd|th)? century", text)
             if match: 
                 century = int(match.group(1))
                 self.year = (century - 1 ) * 100 
                 self.percision = "century"
+
+        elif "circa" in text or "ca" in text or "c." in text: # handles circa dates that are not bc 
+            match = re.search(r"(\d{1,4})", text) 
+            if match: 
+                self.year = int(match.group(1))
+                self.percision = "circa"
         else: # for all other cases, finds instance of any 1-4 numbers and saves as year: ex: 1527
             match = re.match(r"\d{1,4}", text)
             self.year = match.group(1)
