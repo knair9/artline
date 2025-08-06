935f48e5405ed91a72b53bc1a1edb3686adc6095
Kalyani Nair <kalyanisnair05@gmail.com>
2025-08-06 13:44:44 -0700

    load images after fully loaded, bottom image fixed, remove jitter with re-rendering
    Co-authored-by: hnoteboom <hnoteboom@users.noreply.github.com>

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -1,15 +1,20 @@
 import os
 from supabase import create_client, Client
 from dotenv import load_dotenv
+from supabase.client import ClientOptions
 
 load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
-
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-
+# supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase: Client = create_client(SUPABASE_URL,
+    SUPABASE_KEY,
+    options=ClientOptions(
+    postgrest_client_timeout=60,
+    storage_client_timeout=60,
+    schema="public",)
+)
 
 def test_connection():
     # Example: fetch 5 rows from the artifacts table
